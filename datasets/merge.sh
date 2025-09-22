37b52f1eeca2e7ab18b4038c1f5a3287c368e552
boundino <boundino@gmail.com>
2025-09-22 17:53:47 +0200

    force ascii sort order

--- datasets/merge.sh
+++ datasets/merge.sh
@@ -5,7 +5,7 @@ outf=dataset.js
 for i in `ls -d js/*` ; do echo $i >> sorttemp.txt ; done
 
 echo "var dataset = {};" > $outf
-for i in `sort -f sorttemp.txt`
+for i in `LC_COLLATE=C sort -f sorttemp.txt`
 do
     echo $i
     echo >> $outf
